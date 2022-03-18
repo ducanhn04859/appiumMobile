4e036977e48635410da2e9884e7b07fc4b3ca6ec
ducanhn04859 <ducanhn1308@gmail.com>
2022-03-18 13:18:59 +0700

    refactor test_config and install allure reporter

---
+++ test_config/main.js
@@ -0,0 +1,35 @@
+let chai = require("chai")
+exports.config = {
+    runner: 'local',
+    port: 4723,
+    hostname: 'localhost',
+    path: '/wd/hub',
+    logLevel: 'info',
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        require: ['@babel/register'],
+        timeout: 600000
+    },
+    waitforTimeout: 5000,
+    maxInstances: 10,
+    sync: true,
+    specs: [
+        // 'PlayWithMocha.js'
+        // 'api/click.js'
+        // 'api/swiprVertical.js'
+        // 'api/swiprHorizontal.js'
+        // 'src_backup/test_script/LoginPage/TC_005_Login.js',
+        "src/test_script/LoginPage/TC_001_Login.js"
+    ],
+    capabilities: [{
+        "platformName": "Android",
+        "automationName": "UiAutomator2",
+        "udid": "emulator-5554",
+        "appPackage": "com.wdiodemoapp",
+        "appActivity": ".MainActivity"
+    }],
+    before: function () {
+        global.chaiExpect = chai.expect
+    }
+}
